3259172b0f23d3f89ab906a503f9525874971c33
volt901 <volt901@icloud.com>
2023-07-08 18:33:22 +0500

    Initial Commit

---
+++ ShopFigma/ShopFigmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopFigmaApp.swift
+//  ShopFigma
+//
+//  Created by Владимир Ширяев on 08.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShopFigmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
